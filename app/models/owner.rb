b947744e5764e7429b84a66f57803f1040880ff2
jenmcphail <mcphail.jen@gmail.com>
2016-06-15 21:24:22 -0700

    intial commit, adds validations

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,7 +1,10 @@
 class Owner < ActiveRecord::Base
-  # TODO: add validations
-
+  valid_email=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   before_save :normalize_phone_number
+  validates :first_name, presence: true, length: {maximum: 255}
+  validates :last_name, presence: true, length: {maximum: 255}
+  validates :email, presence: true, uniqueness: true, length: {maximum: 255}, format: {with: valid_email}
+
 
   # removes leading 1 and the characters (, ), -, .
   def normalize_phone_number
